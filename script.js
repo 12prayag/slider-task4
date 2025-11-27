d3e56873a8574bc1dd46972aebbbbacfc77800ad
12prayag <prayagpansuriya609@gmail.com>
2025-11-27 17:53:52 +0530

    update the slider

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
-document.addEventListener( 'DOMContentLoaded', function() {
-    var splide = new Splide('.splide',{
-          perPage: 1,
-    }   
-     );
-    splide.mount();
-  } );
+document.addEventListener('DOMContentLoaded', function () {
+      var splide = new Splide('.splide', {
+            perPage: 1,
+            type: 'loop',
+            autoplay: true,
+            interval: 500,
+            interval: 3000,
+
+      }
+      );
+      splide.mount();
+});
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//       new Splide('#splide', {
+//             type: 'loop',
+//             perPage: 3,
+//             focus: 'center',
+//             autoplay: true,
+//             interval: 3000,
+//             flickMaxPages: 3,
+//             updateOnMove: true,
+//             pagination: false,
+//             padding: '10%',
+//             throttle: 300,
+//             breakpoints: {
+//                   1440: {
+//                         perPage: 1,
+//                         padding: '30%'
+//                   }
+//             }
+//       }).mount();
+// });
+
+//////////
+
+var towslider = new Splide('.tow-silder', {
+      type: 'loop',
+      perpage: 1,
+      autoplay:true,
+      
+});
+
+    towslider.mount();
+
+////
+
+var towsilderv2 = new Splide('.tow-silder-v2', {
+      type: 'loop',
+      autoplay: true,
+      perpage: 1,
+      
+});
+
+   towsilderv2.mount();
